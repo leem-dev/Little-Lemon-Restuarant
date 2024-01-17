7298b9b9b7e126c6d319566d59866f566616bfbb
leem-dev <alabitseleem@gmail.com>
2024-01-17 16:48:18 +0100

    work in progress

--- src/components/header-component/header-component.js
+++ src/components/header-component/header-component.js
@@ -1,16 +1,18 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+// import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Logo from "../../assets/Logo .svg"
 import Navigation from "./navigation-component/navigation-component"
 
+
+import "./header-styles.css"
 const Header = () =>    {
     return (
-        <Router>
-            <div>
-            <img src={Logo} alt="Logo" />
-            <Navigation />
+        <>
+            <div className="header-section">
+                <img src={Logo} alt="Logo" className="logo"/>
+                <Navigation />
             </div>
-        </Router>
+        </>
     )
 }
 
--- src/components/header-component/navigation-component/navigation-component.js
+++ src/components/header-component/navigation-component/navigation-component.js
@@ -1,16 +1,37 @@
+import "../header-styles.css";
+
 const Navigation = () =>    {
+
+    const ulStyle = {
+        margin: "0px",
+        padding: "0px",
+        display: "flex",
+        justifyContent: "space-between"
+    }
+
+    const liStyle = {
+        listStyle: "none",
+        fontSize: "17px",
+        fontWeight: "700",
+    }
+
+    const aStyle = {
+        color: "black",
+        textDecoration: "none",
+    }
+
     return (
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/menu">Menu</a></li>
-                <li><a href="/reservation">Reservations</a></li>
-                <li><a href="/order">Order Online</a></li>
-                <li><a href="/login">Login</a></li>
+        <nav className="nav-body" >
+            <ul style={ulStyle}>
+                <li style={liStyle}><a href="/" style={aStyle}>Home</a></li>
+                <li style={liStyle}><a href="/about" style={aStyle}>About</a></li>
+                <li style={liStyle}><a href="/menu" style={aStyle}>Menu</a></li>
+                <li style={liStyle}><a href="/reservation" style={aStyle}>Reservations</a></li>
+                <li style={liStyle}><a href="/order" style={aStyle}>Order Online</a></li>
+                <li style={liStyle}><a href="/login" style={aStyle}>Login</a></li>
             </ul>
         </nav>
     )
 }
 
-export default Navigation
+export default Navigation;
