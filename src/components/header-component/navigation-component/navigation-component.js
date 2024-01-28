9c55f30c80b4a0643bdf5e8317b11c386232191f
leem-dev <alabitseleem@gmail.com>
2024-01-28 23:41:13 +0100

    today

--- src/components/header-component/navigation-component/navigation-component.js
+++ src/components/header-component/navigation-component/navigation-component.js
@@ -6,13 +6,13 @@ import HamIcon from "../../../assets/hamburger-icon.svg";
 import "../header-styles.css";
 
 const Navigation = () => {
-  const ulStyle = {
-    margin: "0px",
-    marginRight: "20px",
-    padding: "0px",
-    display: "flex",
-    justifyContent: "space-between",
-  };
+  // const ulStyle = {
+  //   margin: "0px",
+  //   marginRight: "20px",
+  //   padding: "0px",
+  //   width: "100%",
+  //   display: "flex",
+  // };
 
   const liStyle = {
     listStyle: "none",
@@ -31,53 +31,56 @@ const Navigation = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
+  // const closeMenu = () => {
+  //   setIsMenuOpen(false);
+  // };
 
   return (
-    <div className='dropdown-menu'>
+    <nav className={`dropdown-menu ${isMenuOpen ? "open" : ""}`}>
       <div className='menu-toggle' onClick={toggleMenu}>
-        <img src={HamIcon} alt='ham-icon' className='ham-icon' />
+        {/* <img src={HamIcon} alt='ham-icon' className='ham-icon' /> */}
+        <div className='bar'></div>
+        <div className='bar'></div>
+        <div className='bar'></div>
       </div>
 
-      {isMenuOpen && (
-        <nav className='nav-body-menu' onClick={closeMenu}>
-          <ul style={ulStyle} className='nav-list'>
-            <li style={liStyle}>
-              <Link to='/' style={aStyle} className='nav-link'>
-                Home
-              </Link>
-            </li>
-            <li style={liStyle}>
-              <Link to='/about' style={aStyle} className='nav-link'>
-                About
-              </Link>
-            </li>
-            <li style={liStyle}>
-              <Link to='/menu' style={aStyle} className='nav-link'>
-                Menu
-              </Link>
-            </li>
-            <li style={liStyle}>
-              <Link to='/reservation' style={aStyle} className='nav-link'>
-                Reservations
-              </Link>
-            </li>
-            <li style={liStyle}>
-              <Link to='/order' style={aStyle} className='nav-link'>
-                Order Online
-              </Link>
-            </li>
-            <li style={liStyle}>
-              <Link to='/login' style={aStyle} className='nav-link'>
-                Login
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </div>
+      {/* {isMenuOpen && ( */}
+
+      <ul className={`nav-list ${isMenuOpen ? "visible" : ""}`}>
+        <li style={liStyle}>
+          <Link to='/' style={aStyle} className='nav-link'>
+            Home
+          </Link>
+        </li>
+        <li style={liStyle}>
+          <Link to='/about' style={aStyle} className='nav-link'>
+            About
+          </Link>
+        </li>
+        <li style={liStyle}>
+          <Link to='/menu' style={aStyle} className='nav-link'>
+            Menu
+          </Link>
+        </li>
+        <li style={liStyle}>
+          <Link to='/reservation' style={aStyle} className='nav-link'>
+            Reservations
+          </Link>
+        </li>
+        <li style={liStyle}>
+          <Link to='/order' style={aStyle} className='nav-link'>
+            Order Online
+          </Link>
+        </li>
+        <li style={liStyle}>
+          <Link to='/login' style={aStyle} className='nav-link'>
+            Login
+          </Link>
+        </li>
+      </ul>
+
+      {/* )} */}
+    </nav>
   );
 };
 
