4e5da356845e99096a705713bff86db8daf84141
leem-dev <alabitseleem@gmail.com>
2024-01-29 11:38:37 +0100

    mobile responsiveness

--- src/components/header-component/navigation-component/navigation-component.js
+++ src/components/header-component/navigation-component/navigation-component.js
@@ -1,85 +1,78 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import HamIcon from "../../../assets/hamburger-icon.svg";
 import "../header-styles.css";
 
 const Navigation = () => {
-  // const ulStyle = {
-  //   margin: "0px",
-  //   marginRight: "20px",
-  //   padding: "0px",
-  //   width: "100%",
-  //   display: "flex",
-  // };
-
   const liStyle = {
     listStyle: "none",
     fontSize: "17px",
     fontWeight: "700",
   };
 
-  const aStyle = {
-    color: "black",
-    textDecoration: "none",
-  };
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+    console.log("I am here");
   };
 
-  // const closeMenu = () => {
-  //   setIsMenuOpen(false);
-  // };
+  useEffect(() => {
+    const outsideClick = event => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("click", outsideClick);
+
+    return () => {
+      document.removeEventListener("click", outsideClick);
+    };
+  }, [dropdownRef]);
 
   return (
-    <nav className={`dropdown-menu ${isMenuOpen ? "open" : ""}`}>
+    <nav className={`dropdown-menu ${isMenuOpen ? "open" : ""}`} ref={dropdownRef}>
       <div className='menu-toggle' onClick={toggleMenu}>
-        {/* <img src={HamIcon} alt='ham-icon' className='ham-icon' /> */}
         <div className='bar'></div>
         <div className='bar'></div>
         <div className='bar'></div>
       </div>
 
-      {/* {isMenuOpen && ( */}
-
       <ul className={`nav-list ${isMenuOpen ? "visible" : ""}`}>
         <li style={liStyle}>
-          <Link to='/' style={aStyle} className='nav-link'>
+          <Link to='/' className='nav-link'>
             Home
           </Link>
         </li>
         <li style={liStyle}>
-          <Link to='/about' style={aStyle} className='nav-link'>
+          <Link to='/about' className='nav-link'>
             About
           </Link>
         </li>
         <li style={liStyle}>
-          <Link to='/menu' style={aStyle} className='nav-link'>
+          <Link to='/menu' className='nav-link'>
             Menu
           </Link>
         </li>
         <li style={liStyle}>
-          <Link to='/reservation' style={aStyle} className='nav-link'>
+          <Link to='/reservation' className='nav-link'>
             Reservations
           </Link>
         </li>
         <li style={liStyle}>
-          <Link to='/order' style={aStyle} className='nav-link'>
+          <Link to='/order' className='nav-link'>
             Order Online
           </Link>
         </li>
         <li style={liStyle}>
-          <Link to='/login' style={aStyle} className='nav-link'>
+          <Link to='/login' className='nav-link'>
             Login
           </Link>
         </li>
       </ul>
-
-      {/* )} */}
     </nav>
   );
 };
